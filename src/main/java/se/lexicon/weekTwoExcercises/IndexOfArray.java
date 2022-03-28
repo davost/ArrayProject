2193d277fac101be0638c56c3450b85d6fd643ab
deltagare <bildermejl76@gmail.com>
2022-03-28 16:05:28 +0200

    Week one exercises: new exercise 2 completed

--- src/main/java/se/lexicon/weekTwoExcercises/IndexOfArray.java
+++ src/main/java/se/lexicon/weekTwoExcercises/IndexOfArray.java
@@ -1,7 +1,19 @@
 package se.lexicon.weekTwoExcercises;
-
+import java.util.Arrays;
 public class IndexOfArray {
     public static void indexOf() {
 
+    //2. Create a program and create a method with name ‘indexOf’ which will find and
+    // return the index of an element in the array. If the element doesn’t exist your
+    // method should return -1 as value. Expect output:Index position of number 5 is: 2.
+
+    int[] indexedNums = {21, 15, 5, 4};
+    int index = Arrays.binarySearch(indexedNums, 5);
+
+    if (index == 2) {
+        System.out.println("Index position of number 5 is: " + indexedNums[index]);
+    } else {
+        System.out.println("-5");
+    }
     }
 }
